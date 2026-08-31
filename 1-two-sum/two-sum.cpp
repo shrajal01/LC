22ce6045e51164196055c65db62d6548c10b0559
shrajal01 <shrajal.2428it1420@kiet.edu>
2026-09-01 01:20:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (43.64%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int c = target - nums[i];
+
+            if(mp.find(c) != mp.end())
+                return {mp[c], i};
+
+            mp[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};
